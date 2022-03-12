e9dfe74d7bdfac0212d897625ea107cac1090795
Rahul Seth <57781892+sethr07@users.noreply.github.com>
2022-03-12 17:10:36 +0000

    grabs ips from previous run

--- GrabIPs.py
+++ GrabIPs.py
@@ -1,25 +1,3 @@
-#!/usr/bin/python
-
-# Copyright (C) 2018 Stephen Farrell, stephen.farrell@cs.tcd.ie
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 # grab just the IPs from a censys file 
 # or any other with one json structure per line and an 'ip' key in that dict
 
@@ -37,7 +15,7 @@ parser.add_argument('-o','--output_file',
 args=parser.parse_args()
 
 def usage():
-    print >>sys.stderr, "usage: " + sys.argv[0] + " -i <infile> -o <putfile>" 
+    print (sys.stderr, "usage: " + sys.argv[0] + " -i <infile> -o <putfile>")
     sys.exit(1)
 
 if args.infile is None:
@@ -45,12 +23,11 @@ if args.infile is None:
 
 # checks - can we read/write 
 if not os.access(args.infile,os.R_OK):
-    print >> sys.stderr, "Can't read input file " + args.infile + " - exiting"
+    print (sys.stderr, "Can't read input file " + args.infile + " - exiting")
     sys.exit(1)
 if os.path.isfile(args.outfile) and not os.access(args.outfile,os.W_OK):
-    print >> sys.stderr, "Can't write to output file " + args.outfile + " - exiting"
+    print (sys.stderr, "Can't write to output file " + args.outfile + " - exiting")
     sys.exit(1)
-
 
 out_f=open(args.outfile,"w")
 
@@ -70,8 +47,8 @@ with open(args.infile,'r') as f:
         # print something now and then to keep operator amused
         checkcount += 1
         if checkcount % 100 == 0:
-            print >> sys.stderr, "Grabbing IPs... did: " + str(checkcount) + " most recent IP " + ip + \
-                    " good: " + str(goodcount) + " bad: " + str(badcount)
+            print (sys.stderr, "Grabbing IPs... did: " + str(checkcount) + " most recent IP " + ip + \
+                    " good: " + str(goodcount) + " bad: " + str(badcount))
 
         if checkcount % 1000 == 0:
             gc.collect()
