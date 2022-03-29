eb4ac39ec8254210f821dabdc51a875c15a69012
Rahul Seth <57781892+sethr07@users.noreply.github.com>
2022-03-29 15:37:27 +0100

    updated and working

--- HostPortKeyCount.py
+++ HostPortKeyCount.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Copyright (C) 2018 Stephen Farrell, stephen.farrell@cs.tcd.ie
 # 
@@ -26,6 +26,7 @@
 # TODO: fix this to work with fingerprints.json as well as all-key-fingerprints.json
 # but the latter is good enough!
 
+from asyncore import write
 import sys
 import os
 import tempfile
@@ -42,42 +43,46 @@ from SurveyFuncs import *
 
 
 # command line arg handling 
-parser=argparse.ArgumentParser(description=' Count the host/port combos and the number of actual keys found, from all hosts that do some crypto')
+parser=argparse.ArgumentParser(description=' Count the host/port combos and the number of actual keys found, from all hosts that do some crypto.\n \
+                                            Defualt File is "all-key-fingerprints.')
 parser.add_argument('-f','--file',     
                     dest='fname',
                     help='json file containing key fingerprints')
 args=parser.parse_args()
 
-
-# if this then just print legend
 if args.fname is None:
-    print args
+    print ("No file provided. using default file.")
+
+def_file="all-key-fingerprints.json"
+if(def_file):
+    args.fname = def_file
+else:
+    print("No default file in curr dir.\n")
     sys.exit(0)
 
 # main line processing ...
-
 # we need to pass over all the fingerprints to do our counts
 checkcount=0
 fps_seen=set()
 hostsports=0
 hosts=0
-
 # open file
 fp=open(args.fname,"r")
 
 f=getnextfprint(fp)
 while f:
     fps=f.fprints
+    #print(fps)
     hosts += 1
     for port in fps:
         hostsports += 1
-        #print fps[port]
+        #print (fps[port])
         fps_seen.add(fps[port])
 
     # print something now and then to keep operator amused
     checkcount += 1
     if checkcount % 100 == 0:
-        print >> sys.stderr, "Counting hosts/ports/keys, did: " + str(hosts) 
+        print (sys.stderr, "Counting hosts/ports/keys, did: " + str(hosts))
     if checkcount % 1000 == 0:
         gc.collect()
 
@@ -89,12 +94,12 @@ while f:
 fp.close()
 
 summary_fp=open("hpk_summary.txt","a+")
-print >> summary_fp, "hosts: " + str(hosts) + "\n" + \
+summary_fp.write("hosts: " + str(hosts) + "\n" + \
         "hostsports: " + str(hostsports) + "\n" + \
-        "fps: " + str(len(fps_seen)) 
+        "fps: " + str(len(fps_seen)))
 summary_fp.close()
 
-print >> sys.stderr, "hosts: " + str(hosts) + "\n" + \
+print (sys.stderr, "hosts: " + str(hosts) + "\n" + \
         "hostsports: " + str(hostsports) + "\n" + \
-        "fps: " + str(len(fps_seen)) 
+        "fps: " + str(len(fps_seen)))
 
